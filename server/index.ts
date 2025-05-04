bdca655f0e8ecb3ef937d701bdfc7f024dc19944
Vivek Singh <121109689+Vivek-singh1224@users.noreply.github.com>
2025-05-04 23:36:27 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -59,12 +59,12 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
-  server.listen({
-    port: 5000,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`Server running at http://0.0.0.0:5000`);
-  });
+const port = process.env.PORT || 5000;
+server.listen({
+  port,
+  host: "0.0.0.0",
+  reusePort: true,
+}, () => {
+  log(`Server running at http://0.0.0.0:${port}`);
+});
 })();
